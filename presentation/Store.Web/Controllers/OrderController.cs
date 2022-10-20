d2bed49558eac6276a131eead14f736604edbdcf
Nastiks <polumka1804@gmail.com>
2022-10-20 17:33:17 +0600

    Implement payment by bank card

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using Store.Web.Models;
 using System.Text.RegularExpressions;
 using Store.Messages;
 using Store.Contractors;
+using Store.Web.Contractors;
 
 namespace Store.Web.Controllers
 {
@@ -12,18 +13,21 @@ namespace Store.Web.Controllers
         private readonly IOrderRepository orderRepository;
         private readonly IEnumerable<IDeliveryService> deliveryServices;
         private readonly IEnumerable<IPaymentService> paymentServices;
+        private readonly IEnumerable<IWebContractorService> webContractorServices;
         private readonly INotificationService notificationService;
 
         public OrderController(IJewelryRepository jewelryRepository,
                                IOrderRepository orderRepository,
                                IEnumerable<IDeliveryService> deliveryServices,
                                IEnumerable<IPaymentService> paymentServices,
+                               IEnumerable<IWebContractorService> webContractorServices,
                                INotificationService notificationService)
         {
             this.jewelryRepository = jewelryRepository;
             this.orderRepository = orderRepository;
             this.deliveryServices = deliveryServices;
             this.paymentServices = paymentServices;
+            this.webContractorServices = webContractorServices;
             this.notificationService = notificationService;
         }
 
@@ -259,6 +263,13 @@ namespace Store.Web.Controllers
 
             var form = paymentService.CreateForm(order);
 
+            var webContractorService = webContractorServices.SingleOrDefault(service => service.UniqueCode == uniqueCode);
+            
+            if (webContractorService != null)
+            {
+                return Redirect(webContractorService.GetUri);
+            }
+
             return View("PaymentStep", form);
         }
 
@@ -279,6 +290,13 @@ namespace Store.Web.Controllers
             }
 
             return View("PaymentStep", form);
+        }
+
+        public IActionResult Finish()
+        {
+            HttpContext.Session.RemoveCart();
+
+            return View();
         }
     }
 }
